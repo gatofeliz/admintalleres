0504d9681651fd90f2a2de78f0f35aa79f72defc
Arroyo Flores <damian.pk.89@gmail.com>
2025-03-11 22:41:19 -0600

    fix: código consecutivo

--- app/Filament/Resources/ServiceOrderResource/Pages/CreateServiceOrder.php
+++ app/Filament/Resources/ServiceOrderResource/Pages/CreateServiceOrder.php
@@ -2,11 +2,20 @@
 
 namespace App\Filament\Resources\ServiceOrderResource\Pages;
 
-use App\Filament\Resources\ServiceOrderResource;
 use Filament\Actions;
+use App\Models\ServiceOrder;
 use Filament\Resources\Pages\CreateRecord;
+use App\Filament\Resources\ServiceOrderResource;
 
 class CreateServiceOrder extends CreateRecord
 {
     protected static string $resource = ServiceOrderResource::class;
+
+    protected function mutateFormDataBeforeCreate(array $data): array
+    {
+        $actualOrderServicesCount = ServiceOrder::all()->count();
+        $data['code'] = str_pad($actualOrderServicesCount+1, 6, "0", STR_PAD_LEFT);;
+
+        return $data;
+    }
 }
