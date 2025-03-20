b38a8a11a0d973279d9132312c160d82f27caf1e
Arroyo Flores <damian.pk.89@gmail.com>
2025-03-19 21:55:02 -0600

    chore: quitar email verified

--- app/Filament/Resources/UserResource.php
+++ app/Filament/Resources/UserResource.php
@@ -31,7 +31,6 @@ class UserResource extends Resource
                     ->email()
                     ->required()
                     ->maxLength(255),
-                Forms\Components\DateTimePicker::make('email_verified_at'),
                 Forms\Components\TextInput::make('password')
                         ->password()
                         ->required(fn (string $operation): bool => $operation === 'create')
@@ -53,9 +52,6 @@ class UserResource extends Resource
                     ->searchable(),
                 Tables\Columns\TextColumn::make('email')
                     ->searchable(),
-                Tables\Columns\TextColumn::make('email_verified_at')
-                    ->dateTime()
-                    ->sortable(),
                 Tables\Columns\TextColumn::make('created_at')
                     ->dateTime()
                     ->sortable()
